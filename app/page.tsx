f2b90fad37a750909b4c8eb04f736788bf75e62b
w1cked <manivournthon@gmail.com>
2024-03-23 22:50:15 +0700

    logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 // Import necessary components and modules
 import UserList from "@/components/list-user";
-
+import Image from "next/image";
+import logo from "./sarika.jpg";
 // Function to fetch user data from the API
 async function getUser() {
   try {
@@ -22,9 +23,16 @@ export default async function Home() {
   // Return the main page content
   return (
     // Main page container
-    <div className="container p-10">
-      {/* Title */}
-      <div className="w-full mb-10">
+    <div className="container p-10 items-center">
+      {/* Title and Logo */}
+      <div className="flex items-center space-x-4 mb-4">
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          alt="Picture of the author"
+          className="rounded-full"
+        />
         <h1 className="text-xl font-bold uppercase">
           click on your name to review the text corpus that assigned to you
         </h1>
